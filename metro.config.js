e822aac33540848c7ecdabf08f2ac49f061d43e5
Aviv Zohar <avivzo9@gmail.com>
2025-06-16 23:57:12 +0300

    feat(metro.config): add support for module aliases and ensure default resolver functionality

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,21 @@
-// metro.config.js
-const { getDefaultConfig } = require('@expo/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Add 'mjs' support if not present
-if (!config.resolver.sourceExts.includes('mjs')) {
-    config.resolver.sourceExts.push('mjs');
-}
+const ALIASES = {
+    tslib: path.resolve(__dirname, "node_modules/tslib/tslib.es6.js"),
+};
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+    // Ensure you call the default resolver.
+    return context.resolveRequest(
+        context,
+        // Use an alias if one exists.
+        ALIASES[moduleName] ?? moduleName,
+        platform
+    );
+};
 
 module.exports = config;
